131912ea8f660d904b4ff61d2c7d71ed8c565174
SYN2002 <87717343+SYN2002@users.noreply.github.com>
2023-08-28 02:28:59 +0530

    commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
   return (
     <div className='fixed top-0 z-20 w-full bg-gradient-to-r from-[#1687A7] to-[#1687A7]'>
     <div className='mx-5 flex p-3 justify-between'>
-    <div className=''><a href="#"><p className='text-white font-bold text-4xl max-[640px]:text-3xl font-Montserrat'>Private Share</p></a></div>
+    <div className=''><a href="https://private-share.vercel.app/"><p className='text-white font-bold text-4xl max-[640px]:text-3xl font-Montserrat'>Private Share</p></a></div>
     <div className='mx-3'><a href='https://github.com/SYN2002/PrivateShare' target="_blank" rel="noreferrer"> <i className="fa fa-github" href="github.com/TheAkashRoy"></i></a></div>
     </div>
     </div>
